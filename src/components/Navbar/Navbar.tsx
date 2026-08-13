73b09bdf549191a7d965aac2d716a37b5d9da4ef
Julian Gonzalez <juliandge@outlook.com>
2026-08-12 19:36:06 -0500

    feat: add navigation links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,23 @@ function Navbar() {
     return (
         <nav>
             <h1>Spartan Elite Gym</h1>
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Servicios</a>
+                </li>
+                <li>
+                    <a href="#">Zonas</a>
+                </li>
+                <li>
+                    <a href="#">Entrenadores</a>
+                </li>
+                <li>
+                    <a href="#">Contáctanos</a>
+                </li>
+            </ul>
         </nav>
     )
 }
